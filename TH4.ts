bc76e3c8c294363646b672e20b2fffe2bcb5ef72
thanh duy <you@example.com>
2023-02-13 16:55:14 +0700

    TH4

--- TH4.ts
+++ TH4.ts
@@ -21,7 +21,25 @@ class QuadraticEquation {
         return this.c;
     }
 
-    getDiscriminant(){
-
+    getDiscriminant():number{
+        return Math.pow(this.b,2)-4*this.a*this.c
+    }
+    getRoot1():number{
+        return (-this.b+Math.sqrt(Math.pow(this.b,2)-4*this.a*this.c))/2*this.a
+    }getRoot2():number{
+        return (-this.b-Math.sqrt(Math.pow(this.b,2)-4*this.a*this.c))/2*this.a
+    }hienthi(){
+        if (this.getDiscriminant()>0){
+            console.log(this.getRoot1());
+            console.log(this.getRoot2());
+        }else if (this.getDiscriminant()==0){
+            console.log(this.getRoot1());
+        }else {
+            console.log("The equation has no roots")
+        }
     }
 }
+
+let pt=new QuadraticEquation(1,3,1);
+
+pt.hienthi();
